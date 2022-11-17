eeb7eb9f04836abb9b1e87585a2acf8d0c16794a
Kevin Collins <col16016@byui.edu>
2022-11-17 11:16:31 -0700

    Changed the login file fetch URL

--- test/login.js
+++ test/login.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 const login = async ()=>{
-    const tokenResponse = await fetch('https://dev.stedi.me/login',{
+    const tokenResponse = await fetch('https://dev.stedi.kevincollins.cit262.com/login',{
         method: 'POST',
         body:JSON.stringify({
           userName: 'kecollins4@yahoo.com',
